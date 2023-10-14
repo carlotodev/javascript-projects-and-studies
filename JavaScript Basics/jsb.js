a492bf60d7237b8003cbebffba05f66b00ff308a
carlotodev <carloto.comm@gmail.com>
2023-10-14 18:27:30 -0300

    update

--- JavaScript Basics/jsb.js
+++ JavaScript Basics/jsb.js
@@ -1,3 +1,19 @@
-//  This is an in-line comment.
-/* This is a
-multi-line comment */
+//  Isso é um comentário de uma linha.
+/* Isso é um
+comentário de múltiplas linhas */
+
+//"undefined, null, boolean, string, symbol, bigint, number e object" são os dados fornecidos pelo JavaScript
+
+//variáveis
+
+/*utilizar o código "var" e em seguida o nome da variável para criar uma variável, atribuir um valor à uma variável,
+atribuir o valor de uma variável à outra. (podemos atribuir o valor de uma variável na mesma linha em que a criamos) */
+
+var a;
+a = 7;
+var b;
+b = a;
+//note que ao atribuir o valor de uma variável à outra, a variável com valor já definido sempre vem depois (a direita).
+var myVar = 5;
+var myName = "carloto";
+//string é um tipo de dado de caracteres fornecido entre aspas, no caso acima, myName foi uma variável de string declarada com a string "carloto".
