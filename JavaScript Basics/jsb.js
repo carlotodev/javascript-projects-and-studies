78f2c5020ea5568408b0d327d47c59d2ddac118b
carlotodev <carloto.comm@gmail.com>
2023-10-14 19:45:24 -0300

    update

--- JavaScript Basics/jsb.js
+++ JavaScript Basics/jsb.js
@@ -1,19 +1,27 @@
 //  Isso é um comentário de uma linha.
+
 /* Isso é um
 comentário de múltiplas linhas */
 
-//"undefined, null, boolean, string, symbol, bigint, number e object" são os dados fornecidos pelo JavaScript
+//"undefined, null, boolean, string, symbol, bigint, number e object" são os 8 dados fornecidos pelo JavaScript.
 
 //variáveis
 
 /*utilizar o código "var" e em seguida o nome da variável para criar uma variável, atribuir um valor à uma variável,
-atribuir o valor de uma variável à outra. (podemos atribuir o valor de uma variável na mesma linha em que a criamos) */
+atribuir o valor de uma variável à outra. (podemos atribuir o valor de uma variável na mesma linha em que a declaramos)*/
 
 var a;
-a = 7;
 var b;
+a = 7;
 b = a;
 //note que ao atribuir o valor de uma variável à outra, a variável com valor já definido sempre vem depois (a direita).
 var myVar = 5;
 var myName = "carloto";
 //string é um tipo de dado de caracteres fornecido entre aspas, no caso acima, myName foi uma variável de string declarada com a string "carloto".
+let catName = "Oliver";
+let carSound = "Meow!";
+/*o código let também pode ser utilizado para declarar uma variável, mas ele não permite que voce utilize-o novamente para redeclarar uma variável,
+diferentemente do código var. (variáveis criada com o código let ainda permitem que seus valores mudem)*/
+const FAV_PET = "Cats"
+/*o código const também pode ser utilizado para declarar uma variável, diferentemente dos códigos var e let, o código const declara variáveis contstantes,
+essas variáveis são somente de leitura e não permitem que seus valores sejam reatribuidos nunca (damos o nome dessas variáveis de variáveis imutáveis)*/
